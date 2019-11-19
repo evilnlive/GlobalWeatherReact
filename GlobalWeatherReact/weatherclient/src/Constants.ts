81a05772357b1ee38544618de7e6bc9ea35d66db
Emil Paulsen <emil.paulsen@gmail.com>
2019-11-19 22:00:40 +0100

    Add weather details components and types

---
+++ GlobalWeatherReact/weatherclient/src/Constants.ts
@@ -0,0 +1,7 @@
+export class Constants {
+    static locationAPIUrl = 'https://dataservice.accuweather.com/locations/v1';
+    static citySearchAPIUrl = 'https://dataservice.accuweather.com/locations/v1/cities/';
+    static currentConditionsAPIUrl = 'https://dataservice.accuweather.com/currentconditions/v1';
+    static weatherIconUrl = 'http://developer.accuweather.com/sites/default/files/';
+    static apiKey = 'by05roANzDcNSOdzOA8dT8OphA0BgBxQ';
+}
--- GlobalWeatherReact/weatherclient/src/components/Home.tsx
+++ GlobalWeatherReact/weatherclient/src/components/Home.tsx
@@ -1,28 +1,107 @@
-import React, { Component } from 'react';
-import logo from '../logo.svg';
-import '../App.css';
+import React from 'react';
+import Form from './Form';
+import WeatherDetails from './WeatherDetails';
+import { Weather } from '../types/Weather';
+import { Country } from '../types/Country';
+import { City } from '../types/City';
+import { Constants } from '../Constants';
+import { CurrentCondition } from '../types/CurrentCondition';
+
+interface IState {
+    weather: Weather,
+    countries: Country[],
+    city?: City
+}
 
 class Home extends React.Component {
-  render() {
-      return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+    public state: IState = {
+        weather: {
+            error: ""
+        } as Weather,
+        countries: [],
+        city: undefined
+    }
+
+    render() {
+        return (
+            <div className="container content panel">
+                <div className="container">
+                    <div className="row">
+                        <div className="form-container">
+                            <WeatherDetails weather={this.state.weather} />
+                            <Form getWeather={this.getWeather} countries={this.state.countries} />
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     }
+
+    async componentDidMount() {
+        try {
+            const countries = await this.getCountries();
+            await this.setStateAsync({ countries: countries } as IState);
+        } catch (error) {
+        }
+    }
+
+    async setStateAsync(state: IState) {
+        return new Promise((resolve: any) => {
+            this.setState(state, resolve);
+        });
+    }
+
+    async getCountries(): Promise<Country[]> {
+        try {
+            const res = await fetch (`${Constants.locationAPIUrl}/countries?apikey=${Constants.apiKey}`);
+            return await res.json() as Country[];
+        } catch (error) {
+            console.log(error);
+            return [];
+        }
+    }
+
+    async getCity(searchText: string, countryCode: string): Promise<City> {
+        const res = await fetch(`${Constants.locationAPIUrl}/cities/${countryCode}/search?apikey=${Constants.apiKey}&q=${searchText}`);
+        const cities = await res.json() as City[];
+        if (cities.length > 0)
+            return cities[0];
+        return {} as City;
+    }
+
+    async getCurrentConditions(city: City) {
+        try {
+            const res = await fetch(`${Constants.currentConditionsAPIUrl}/${city.Key}?apikey=${Constants.apiKey}`);
+            const currentConditions = await res.json() as CurrentCondition[];
+            if (currentConditions.length > 0) {
+                const weather = new Weather(currentConditions[0], city);
+                await this.setStateAsync({
+                    weather: weather,
+                    city: city
+                } as IState);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+        return {} as Weather;
+    }
+
+    getWeather = async (e: any, countryCode: string, searchText: string) => {
+        e.preventDefault();
+        if (!countryCode && !searchText) {
+            await this.setStateAsync
+                 ({ weather: { error: "Please enter the value." } } as IState);
+            return;
+        }
+        try {
+            const city = await this.getCity(searchText, countryCode);
+            if (city.Key) {
+               await this.getCurrentConditions(city);
+            }
+        } catch (err) {
+            await this.setStateAsync({ weather: { error: err } } as IState);
+        }
+    };
 }
 
 export default Home;
